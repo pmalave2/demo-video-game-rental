6da8f8f14145ab1abc74c631d7b00937c9b2d0a5
pavele <pmalave2@hotmail.com>
2023-05-15 21:37:35 +0200

    init

---
+++ src/test/java/com/videogamerental/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.videogamerental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
